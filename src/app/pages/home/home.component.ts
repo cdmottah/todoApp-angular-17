d3197fa46d197a58c0aa2d296cfbdc02f61457d3
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-21 23:08:28 -0500

    feat(home): add signals for task management and initial course lessons
    Introduce signal-based task management for the HomeComponent, including:
    - Definition of a `task` model interface.
    - Implementation of add, update, and delete functionality for tasks using signals.
    - Enhanced UI to display task lists and toggle their completion states.
    - Example of `*ngFor` usage for dynamic task rendering.
    
    This commit also incorporates the first 15 classes from the course, focusing on signals and object handling.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,55 @@ import { Component, signal } from '@angular/core';
 })
 export class HomeComponent {
 
-  readonly tasks = signal([
-    'learn angular 17',
-    'learn testing for routes',
-    'typescript in deep'
-
+  readonly tasks = signal<task[]>([
+    {
+      id: Date.now(),
+      title: 'learn angular 17',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'typescript in deep',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'learn testing for routes',
+      completed: false
+    }
   ])
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask)
+  }
+
+  addTask(title: string) {
+    const newTask: task = {
+      id: Date.now(),
+      title,
+      completed: false
+    }
+    this.tasks.update((tasks) => [...tasks, newTask])
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position == index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+
+        }
+        return task
+      })
+    })
+  }
 }
